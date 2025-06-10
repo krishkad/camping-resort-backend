72c021ebcdcec26126c4e63cfe1d02e06b5c8192
krrish <krishkadam88@gmail.com>
2025-06-10 17:55:25 +0530

    api/index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,24 +14,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: `${process.env.CORS_ORIGIN_URL!}`,
+    origin: process.env.CORS_ORIGIN_URL!,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(cookieParser());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN_URL!);
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 app.use("/api/booking", bookingRoute);
 app.use("/api/auth", authRoute);
