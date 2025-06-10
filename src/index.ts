71f3597aff2a72d2fa62fd09a57f066378313059
krrish <krishkadam88@gmail.com>
2025-06-10 17:25:56 +0530

    content type

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,8 @@ app.use(
   cors({
     origin: `${process.env.CORS_ORIGIN_URL!}`,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(cookieParser());
